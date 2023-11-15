9d7d0fc7bc6631653934a63678aa778187b06f9b
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-11-15 16:37:01 +0300

    Add data.py to task 2.1.2

--- 2. Processing and verification of requests/2.1 Processing HTTP requests/task_2_1_2/app/helper.py
+++ 2. Processing and verification of requests/2.1 Processing HTTP requests/task_2_1_2/app/helper.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException
+from app.data import sample_products
 
 
 def get_product_by_id(product_id) -> dict:
-    product = [product for product in sample_products if product["product_id"] == product_id]
+    product = [
+        product for product in sample_products if product["product_id"] == product_id]
     if product:
         return product[0]
     else:
@@ -18,42 +20,3 @@ def get_products_by_parameters(keyword: str, category: str = None, limit: int = 
         filter_list = list(
             filter(lambda product: keyword in product["name"].lower(), sample_products))
     return filter_list[:limit]
-
-
-sample_product_1 = {
-    "product_id": 123,
-    "name": "Smartphone",
-    "category": "Electronics",
-    "price": 599.99
-}
-
-sample_product_2 = {
-    "product_id": 456,
-    "name": "Phone Case",
-    "category": "Accessories",
-    "price": 19.99
-}
-
-sample_product_3 = {
-    "product_id": 789,
-    "name": "Iphone",
-    "category": "Electronics",
-    "price": 1299.99
-}
-
-sample_product_4 = {
-    "product_id": 101,
-    "name": "Headphones",
-    "category": "Accessories",
-    "price": 99.99
-}
-
-sample_product_5 = {
-    "product_id": 202,
-    "name": "Smartwatch",
-    "category": "Electronics",
-    "price": 299.99
-}
-
-sample_products = [sample_product_1, sample_product_2,
-                   sample_product_3, sample_product_4, sample_product_5]
