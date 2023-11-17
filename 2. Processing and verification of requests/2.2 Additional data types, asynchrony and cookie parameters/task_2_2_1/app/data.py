1ed36c6384e21059f81d69f1ac5bc80da52cb36d
vypiemzalyubov <dima.rza90@mail.ru>
2023-11-17 21:03:52 +0300

    Updaye task 2.2.1

--- 2. Processing and verification of requests/2.2 Additional data types, asynchrony and cookie parameters/task_2_2_1/app/data.py
+++ 2. Processing and verification of requests/2.2 Additional data types, asynchrony and cookie parameters/task_2_2_1/app/data.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 
+
 user_data: dict = {"username": "John", "password": "Travolta"}
 user_db: list[User] = [User(**user_data)]
 sessions: dict = {}
--- 2. Processing and verification of requests/2.2 Additional data types, asynchrony and cookie parameters/task_2_2_1/app/main.py
+++ 2. Processing and verification of requests/2.2 Additional data types, asynchrony and cookie parameters/task_2_2_1/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Cookie, Response, HTTPException
 from app.models.user import User
 from app.data import user_db, sessions
 
+
 app = FastAPI(title="Task 2.2.1")
 
 users = {}
@@ -13,14 +14,15 @@ async def login_user(user: User, response: Response):
         if person.username == user.username and person.password == user.password:
             session_token = "fake_token"
             sessions[session_token] = user
-            response.set_cookie(key="session_token", value=session_token, httponly=True)
+            response.set_cookie(key="session_token",
+                                value=session_token, httponly=True)
             return {"message": "cookies set"}
         else:
-            raise HTTPException(status_code=404, detail="User not found")            
+            raise HTTPException(status_code=404, detail="User not found")
 
 
 @app.get("/user")
-async def get_user(session_token = Cookie()):
+async def get_user(session_token=Cookie()):
     user = sessions.get(session_token)
     if user:
         return user.dict()
