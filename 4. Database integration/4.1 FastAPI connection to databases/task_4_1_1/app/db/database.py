10132939d2bdcd3c1a48219166abdbece9928f47
vypiemzalyubov <dima.rza90@mail.ru>
2023-11-28 20:44:56 +0300

    Update task

--- 4. Database integration/4.1 FastAPI connection to databases/task_4_1_1/app/db/database.py
+++ 4. Database integration/4.1 FastAPI connection to databases/task_4_1_1/app/db/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.db.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+# from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+# DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -20,3 +20,5 @@ def get_db():
 
 
 Base = declarative_base()
+
+print(engine)
