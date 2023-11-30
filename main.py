d834fb557bc60b03bdea90b7c88b319f6e81b480
vypiemzalyubov <dima.rza90@mail.ru>
2023-11-30 22:02:29 +0300

    Update main

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from databases import Database
+from sqlalchemy import String, create_engine
+from pydantic import BaseModel
+from typing import Optional
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-# Пример пользовательских данных (для демонстрационных целей) 
-fake_users = {
-    1: {"username": "john_doe", "email": "john@example.com"},
-    2: {"username": "jane_smith", "email": "jane@example.com"},
-}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
 
-# Конечная точка для получения информации о пользователе по ID
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
-    if user_id in fake_users:
-        return fake_users[user_id]
-    return {"error": "User not found"}
+app = FastAPI(lifespan=lifespan)
+
+# URL для PostgreSQL (измените его под свою БД)
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
+
+database = Database(DATABASE_URL)
+
+
+# Модель User для валидации входных данных
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+
+# Модель User для валидации исходящих данных - чисто для демонстрации (обычно входная модель шире чем выходная, т.к. на вход мы просим, например, пароль, который обратно не возвращаем, и другое, что не обязательно возвращать) 
+class UserReturn(BaseModel):
+    username: str
+    email: str
+    id: Optional[int] = None
+
+
+# тут устанавливаем условия подключения к базе данных и отключения - можно использовать в роутах контекстный менеджер async with Database(...) as db: etc
+# @app.on_event("startup")
+# async def startup_database():
+#     await database.connect()
+
+# @app.on_event("shutdown")
+# async def shutdown_database():
+#     await database.disconnect()
+    
+    
+# создание роута для создания юзеров
+@app.post("/users/", response_model=UserReturn)
+async def create_user(user: UserCreate):
+    query = "INSERT INTO users (username, email) VALUES (:username, :email) RETURNING id"
+    values = {"username": user.username, "email": user.email}
+    try:
+        user_id = await database.execute(query=query, values=values)
+        return {**user.dict(), "id": user_id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to create user")
