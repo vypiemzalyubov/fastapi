809893715212c7c92055d881320ac6360239f271
vypiemzalyubov <dima.rza90@mail.ru>
2023-12-25 21:18:37 +0300

    Update shum

---
+++ shum/models/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, text
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Roles(Base):
+    __tablename__ = "role"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
+
+
+class Users(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    role_id = Column(Integer, ForeignKey("role.id", ondelete="CASCADE"))
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
