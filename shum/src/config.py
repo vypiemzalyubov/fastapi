1001f4adf2c2132bc2dc0b54c73e38e3385e7fae
Dmitriy Nikolaev <dnikolaev@tradingview.com>
2023-12-29 12:07:15 +0300

    Update project

--- shum/src/config.py
+++ shum/src/config.py
@@ -10,3 +10,6 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
+
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
